b9890e0b24e49871add2fc1487450ba64cb904fb
Tim Clements <thclements@gmail.com>
2022-07-08 15:56:50 -0700

    Update plotting script

--- src/08-plot-figure-4.jl
+++ src/08-plot-figure-4.jl
@@ -20,6 +20,7 @@ minlon = floor(minimum(fitdf[:,:LON]))
 maxlon = ceil(maximum(fitdf[:,:LON]))
 minlat = floor(minimum(fitdf[:,:LAT]))
 maxlat = ceil(maximum(fitdf[:,:LAT])) - 0.75 
+Sind = findall(fitdf[:,:D3] .< 10.)
 
 # add VS30 values to PGV 
 # load VS30 data from USGS https://earthquake.usgs.gov/data/vs30/
@@ -192,8 +193,8 @@ GMT.scatter!(
 )
 
 # scatter ratio of hydro to temperature 
-tempcomp = abs.(fitdf[:,:E4]) ./ (abs.(fitdf[:,:E4]) .+  abs.(fitdf[:,:E2]))
-Eind = findall((fitdf[:,:E2] .< 0.0) .& (fitdf[:,:E4] .> 0.))
+tempcomp = abs.(fitdf[:,:D4]) ./ (abs.(fitdf[:,:D4]) .+  abs.(fitdf[:,:D2]))
+Dind = findall((fitdf[:,:D2] .< 0.0) .& (fitdf[:,:D4] .> 0.))
 C = GMT.makecpt(T=(-100.0,100.0,1.0), cmap=:polar)
 GMT.grdimage(
     "@earth_relief_15s",
@@ -210,15 +211,19 @@ GMT.coast!(
     ocean=:white,
     N="a",
 )
-GMT.scatter!(
-    fitdf[Eind,:LON],
-    fitdf[Eind,:LAT],
-    markeredgecolor=:black,
-    zcolor=(tempcomp[Eind] .- 0.5) .* 200,
-    colormap=C,
-    alpha=5,
-    markersize="6p",
-)
+# workaround for loop 
+# GMT only allows transparency set as a scalar
+for ii in 1:length(Sind)
+    GMT.scatter!(
+        [fitdf[Sind[ii],:LON]],
+        [fitdf[Sind[ii],:LAT]],
+        markeredgecolor=:black,
+        zcolor=[(tempcomp[Sind[ii]] .- 0.5) .* 200],
+        colormap=C,
+        alpha=min(100 - round(Int, fitdf[Sind[ii],:r2D] .* 100), 99),
+        markersize="4p",
+    )
+end
 rect = [LAminlon LAminlat; LAminlon LAmaxlat; LAmaxlon LAmaxlat; LAmaxlon LAminlat; LAminlon LAminlat];
 GMT.plot!(
     rect, 
@@ -270,14 +275,233 @@ GMT.coast!(
     ocean=:white,
     N="a",
 )
+for ii in 1:length(Sind)-1
+    GMT.scatter!(
+        [fitdf[Sind[ii],:LON]],
+        [fitdf[Sind[ii],:LAT]],
+        markeredgecolor=:black,
+        zcolor=[(tempcomp[Sind[ii]] .- 0.5) .* 200],
+        colormap=C,
+        alpha=min(100 - round(Int, fitdf[Sind[ii],:r2D] .* 100), 99),
+        markersize="4p",
+    )
+end
+
 GMT.scatter!(
-    fitdf[Eind,:LON],
-    fitdf[Eind,:LAT],
+    [fitdf[Sind[end],:LON]],
+    [fitdf[Sind[end],:LAT]],
     markeredgecolor=:black,
-    zcolor=(tempcomp[Eind] .- 0.5) .* 200,
+    zcolor=[(tempcomp[Sind[end]] .- 0.5) .* 200],
     colormap=C,
-    alpha=0,
+    alpha=min(100 - round(Int, fitdf[Sind[end],:r2D] .* 100), 99),
     show=true,
     markersize="4p",
     savefig=joinpath(@__DIR__,"../data/FINAL-FIGURES/Hydro-Thermal-Mixing-map.png"),
+)
+
+gray = GMT.makecpt(color=150, range=(-10000,10000), no_bg=:true);
+hotmap = GMT.makecpt(range=(-4.0,1.0,20,:number),log=true,cmap=:hot)
+GMT.basemap(
+    region=(minlon,maxlon,minlat,maxlat),
+    J=:guess,
+)
+GMT.grdimage!(
+    "@earth_relief_15s",
+    cmap=gray, 
+    shade=true,
+    coast=true,
+    colorbar=false,
+    t=50,
+)
+GMT.coast!(
+    shore=true, 
+    ocean=:white,
+    N="a",
+)
+for ii in eachindex(Sind)
+    GMT.scatter!(
+        [fitdf[Sind[ii],:LON]],
+        [fitdf[Sind[ii],:LAT]],
+        zcolor=[fitdf[Sind[ii],:D3]],
+        markeredgecolor=:black,
+        colorbar=false,
+        alpha=min(100 - round(Int, fitdf[Sind[ii],:r2D] .* 100), 99),
+        cmap=hotmap,
+    )
+end
+GMT.colorbar!(
+    cmap=hotmap,
+    log=true,
+    frame=(annot=:auto, ticks=:auto, xlabel="Diffusivity [m^2/s]"),
+)
+
+# inset map 
+# lat/lon for LA map 
+CVminlon = -121.55
+CVmaxlon = -118.75 
+CVminlat = 35.95 
+CVmaxlat = 36.5 
+rectCV = [CVminlon CVminlat; CVminlon CVmaxlat; CVmaxlon CVmaxlat; CVmaxlon CVminlat; CVminlon CVminlat];
+GMT.plot!(
+    rectCV, 
+    region=(minlon,maxlon,minlat,maxlat), 
+    lw=1,
+    ls=:dashed
+)
+GMT.plot!(
+    [CVminlon CVminlat; -124.6 32.9],
+    region=(minlon,maxlon,minlat,maxlat), 
+    lw=1,
+    ls=:dashed,
+    alpha=15,
+)
+GMT.plot!(
+    [CVmaxlon CVminlat; -117.5 32.9],
+    region=(minlon,maxlon,minlat,maxlat), 
+    lw=1,
+    ls=:dashed, 
+    alpha=15,
+)
+ttCV = GMT.mapproject(region=(CVminlon,CVmaxlon,CVminlat,CVmaxlat), proj=:merc, figsize=9, map_size=true);
+mapW = ttCV[1];   mapH = ttCV[1]
+GMT.basemap!(inset=(size=(mapW, mapH), anchor=:TR, width=1, offset=(-0.5, -7.5), save="xx001"))
+tCV = readdlm("xx001")
+GMT.grdimage!(
+    "@earth_relief_15s",
+    cmap=gray, 
+    shade=true,
+    coast=true,
+    colorbar=false,
+    region=(CVminlon,CVmaxlon,CVminlat,CVmaxlat),
+    J=:merc,
+    xshift=tCV[1], yshift=tCV[2],
+    t=20,
+)
+GMT.coast!(
+    shore=true, 
+    ocean=:lightskyblue,
+    N="a",
+    colorbar=false,
+)
+for ii in 1:length(Sind)- 1
+    GMT.scatter!(
+        [fitdf[Sind[ii],:LON]],
+        [fitdf[Sind[ii],:LAT]],
+        zcolor=[fitdf[Sind[ii],:D3]],
+        markeredgecolor=:black,
+        colorbar=false,
+        alpha=min(100 - round(Int, fitdf[Sind[ii],:r2D] * 100), 99),
+        cmap=hotmap,
+    )
+end
+GMT.scatter!(
+    [fitdf[Sind[end],:LON]],
+    [fitdf[Sind[end],:LAT]],
+    zcolor=[fitdf[Sind[end],:D3]],
+    markeredgecolor=:black,
+    colorbar=false,
+    alpha=min(100 - round(Int, fitdf[Sind[end],:r2D] * 100) / 3, 99),
+    cmap=hotmap,
+    show=true,
+    savefig=joinpath(@__DIR__,"../data/FINAL-FIGURES/Drained-Diffusivity-CV.png"),
+)
+
+
+GMT.basemap(
+    region=(minlon,maxlon,minlat,maxlat),
+    J=:guess,
+)
+GMT.grdimage!(
+    "@earth_relief_15s",
+    cmap=gray, 
+    shade=true,
+    coast=true,
+    colorbar=false,
+    t=50,
+)
+GMT.coast!(
+    shore=true, 
+    ocean=:white,
+    N="a",
+)
+for ii in eachindex(Sind)
+    GMT.scatter!(
+        [fitdf[Sind[ii],:LON]],
+        [fitdf[Sind[ii],:LAT]],
+        zcolor=[fitdf[Sind[ii],:D3]],
+        markeredgecolor=:black,
+        colorbar=false,
+        alpha=min(100 - round(Int, fitdf[Sind[ii],:r2D] .* 100), 99),
+        cmap=hotmap,
+    )
+end
+GMT.colorbar!(
+    cmap=hotmap,
+    log=true,
+    frame=(annot=:auto, ticks=:auto, xlabel="Diffusivity [m^2/s]"),
+)
+rectLA = [LAminlon LAminlat; LAminlon LAmaxlat; LAmaxlon LAmaxlat; LAmaxlon LAminlat; LAminlon LAminlat];
+GMT.plot!(
+    rectLA, 
+    region=(minlon,maxlon,minlat,maxlat), 
+    lw=1,
+    ls=:dashed
+)
+GMT.plot!(
+    [LAminlon LAmaxlat; -119.65 38.9],
+    region=(minlon,maxlon,minlat,maxlat), 
+    lw=1,
+    ls=:dashed,
+    alpha=15,
+)
+GMT.plot!(
+    [LAmaxlon LAmaxlat; -114.55 38.9],
+    region=(minlon,maxlon,minlat,maxlat), 
+    lw=1,
+    ls=:dashed, 
+    alpha=15,
+)
+
+# inset map southern california
+ttLA = GMT.mapproject(region=(LAminlon,LAmaxlon,LAminlat,LAmaxlat), proj=:merc, figsize=6, map_size=true);
+mapW = ttLA[1];   mapH = ttLA[1]
+GMT.basemap!(inset=(size=(mapW, mapH), anchor=:TR, width=1, offset=(-6.75, -13.125), save="xx000"))
+tLA = readdlm("xx000")
+GMT.grdimage!(
+    "@earth_relief_15s",
+    cmap=gray, 
+    shade=true,
+    coast=true,
+    colorbar=false,
+    region=(LAminlon,LAmaxlon,LAminlat,LAmaxlat),
+    J=:merc,
+    xshift=tLA[1], yshift=tLA[2],
+)
+GMT.coast!(
+    shore=true, 
+    ocean=:lightskyblue,
+    N="a",
+    colorbar=false,
+)
+for ii in 1:length(Sind)- 1
+    GMT.scatter!(
+        [fitdf[Sind[ii],:LON]],
+        [fitdf[Sind[ii],:LAT]],
+        zcolor=[fitdf[Sind[ii],:D3]],
+        markeredgecolor=:black,
+        colorbar=false,
+        alpha=min(100 - round(Int, fitdf[Sind[ii],:r2D] * 100), 99),
+        cmap=hotmap,
+    )
+end
+GMT.scatter!(
+    [fitdf[Sind[end],:LON]],
+    [fitdf[Sind[end],:LAT]],
+    zcolor=[fitdf[Sind[end],:D3]],
+    markeredgecolor=:black,
+    colorbar=false,
+    alpha=min(100 - round(Int, fitdf[Sind[end],:r2D] * 100) / 4, 99),
+    cmap=hotmap,
+    show=true,
+    savefig=joinpath(@__DIR__,"../data/FINAL-FIGURES/Drained-Diffusivity-SC.png"),
 )
