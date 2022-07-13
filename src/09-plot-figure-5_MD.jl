11ba268750438dea48459081e1de6d33a3624a4b
Marine Denolle <mdenolle@uw.edu>
2022-07-13 10:12:58 -0700

    update long term

--- src/09-plot-figure-5_MD.jl
+++ src/09-plot-figure-5_MD.jl
@@ -4,6 +4,8 @@ using DSP , GLM, Interpolations, Statistics, StatsModels
 using GMT  
 using Plots
 
+
+####### Some functions
 function DVVslope(df::DataFrame,ind::AbstractArray)
     t = (df[ind,:DATE] .- df[ind[1],:DATE]) ./ Day(1) .+ 1
 
@@ -17,15 +19,6 @@ function DVVslope(df::DataFrame,ind::AbstractArray)
     interSTD, slopeSTD = stderror(ols)
     return inter, slope, rtwo
 end
-
-"""
-    smooth_withfiltfilt(A::AbstractArray; window_len::Int=7, window::Symbol=:bartlett)
-Apply DSP.filtfilt() to smooth the waveform.
-# Arguments
-- "A::AbstractArray": periodogram of spectrum
-- "window_len::Int=7": window length
-- "window::Symbol=:bartlett": window type: (see https://juliadsp.github.io/DSP.jl/stable/windows/)
-"""
 function smooth_withfiltfilt(A::AbstractArray; window_len::Int=11, window::Symbol=:rect)
     w = getfield(DSP.Windows, window)(window_len)
     A = DSP.filtfilt(w ./ sum(w), A)
@@ -44,53 +37,10 @@ LAmaxlon = -116.5
 LAminlat = 32.75 
 LAmaxlat = 34.75 
 
+# # plot maps 2-4 Hz 
+freqmin = 2.0
+freqmax = 4.0
 
-######################### play with Grace data ################## 
-# load GRACE data 
-# from http://www2.csr.utexas.edu/grace/RL06_mascons.html
-filename = joinpath(@__DIR__,"../data/CSR_GRACE_GRACE-FO_RL06_Mascons_all-corrections_v02.nc")
-tlwe = Date(2002,1,1) .+ Day.(floor.(ncread(filename,"time")))
-lon = ncread(filename,"lon")
-lat = ncread(filename,"lat")
-# lwe ./= 100 # convert to m 
-ind2004 = findfirst(tlwe .== Date(2004,10,16))
-ind2005e = findfirst(tlwe .== Date(2005,5,16))
-ind2005f = findfirst(tlwe .== Date(2005,10,16))
-ind2011 = findfirst(tlwe .== Date(2011,9,16))
-ind2016 = findfirst(tlwe .== Date(2016,8,21))
-ind2020 = findfirst(tlwe .== Date(2020,10,16))
-GG=ncread(filename,"lwe_thickness")
-
-# calculate the standard deviation
-Gstd=zeros(size(GG[:,:,1]))
-for i in 1:length(GG[:,1,1])
-    for j in 1:length(GG[1,:,1])
-        Gstd[i,j]=std(GG[i,j,:])
-    end
-end
-# standard deviation 
-Plots.heatmap(lon,lat,Gstd',clim=(0,20),
-xlim=(360-125,360-115),ylim=(32,40))
-
-# standard deviation 
-Plots.heatmap(lon,lat,(abs.(Gmax)./Gstd)',clim=(0,2),
-xlim=(360-125,360-115),ylim=(32,40))
-
-Gmax=GG[:,:,ind2004]-GG[:,:,ind2005e]
-
-G2004a=sum(GG[:,:,ind2004:ind2005e],dims=3)
-G2012a=sum(GG[:,:,ind2011:ind2016],dims=3)
-# p=heatmap(lat,lon,G2004a[:,:,1],clim=(-100,100),xlim=(31,40),ylim=(-125+360,-114+360),c =:redsblues)
-G2004 = gmtread(filename,layer=ind2004,varname="lwe_thickness")
-G2005e = gmtread(filename,layer=ind2005e,varname="lwe_thickness")
-G2005f = gmtread(filename,layer=ind2005f,varname="lwe_thickness")
-G2011 = gmtread(filename,layer=ind2011,varname="lwe_thickness")
-G2016 = gmtread(filename,layer=ind2016,varname="lwe_thickness")
-G2020 = gmtread(filename,layer=ind2020,varname="lwe_thickness")
-
-
-# load fitted values 
-fitdf = Arrow.Table(joinpath(@__DIR__,"../data/hydro-model-90-day.arrow")) |> Arrow.columntable |> DataFrame
 
 ######################### play with temperature data ##################
 # load temperature values 
@@ -119,13 +69,12 @@ for year in 2001:2022
     for i in 1:length(ppt[:,1,1])
         for j in 1:length(ppt[1,:,1])
             crap=ppt[i,j,ind]
-            # print(i,j,sum(crap[crap.>0]))
             cummprecip[i,j,year-2000]=sum(crap[crap.>0])
         end
     end
 end
 # cool plot of rain in 2004-2005 winter
-Plots.heatmap(plon,plat,cummprecip[:,:,4]/1000,xlim=(-125,-115),clim=(0,2))
+# Plots.heatmap(plon,plat,cummprecip[:,:,4]/1000,xlim=(-125,-115),clim=(0,2))
 
 # get variability in precipitation
 precipmean=zeros(size(cummprecip[:,:,1]))
@@ -136,11 +85,23 @@ for i in 1:length(cummprecip[:,1,1])
         precipmean[i,j]=mean(crap)
     end
 end
-Plots.heatmap(plon,plat,precipmean/1000,xlim=(-125,-115),clim=(0,2))
+# Plots.heatmap(plon,plat,precipmean/1000,xlim=(-125,-115),clim=(0,2))
 
-## plot to show the ratio of precipitation over mean precipitation.
-Plots.heatmap(plon,plat,-log10.(cummprecip[:,:,4]./precipmean),
-xlim=(-125,-115),clim=(-0.7,0.7),color=:bluesreds)
+# ## plot to show the ratio of precipitation over mean precipitation.
+# Plots.heatmap(plon,plat,-log10.(cummprecip[:,:,4]./precipmean),
+# xlim=(-125,-115),clim=(-0.7,0.7),color=:bluesreds)
+
+# mean 2005-2020 data 
+precipmean_longterm=zeros(size(cummprecip[:,:,1]))
+crap=0
+for i in 1:length(cummprecip[:,1,1])
+    for j in 1:length(cummprecip[1,:,1])
+        crap=cummprecip[i,j,5:end]
+        precipmean_longterm[i,j]=mean(crap)
+    end
+end
+# Plots.heatmap(plon,plat,-log10.(precipmean_drought[:,:]./precipmean),
+# xlim=(-125,-115),clim=(-0.3,0.3),color=:bluesreds)
 
 # mean 2012-2016 data 
 precipmean_drought=zeros(size(cummprecip[:,:,1]))
@@ -151,23 +112,47 @@ for i in 1:length(cummprecip[:,1,1])
         precipmean_drought[i,j]=mean(crap)
     end
 end
-Plots.heatmap(plon,plat,-log10.(precipmean_drought[:,:]./precipmean),
-xlim=(-125,-115),clim=(-0.3,0.3),color=:bluesreds)
+########################## seismic dv/v stuff #############
 
-#######################################
-
-# plot maps 2-4 Hz 
-freqmin = 2.0
-freqmax = 4.0
+# load fitted values dvv 
+fitdf = Arrow.Table(joinpath(@__DIR__,"../data/hydro-model-90-day.arrow")) |> Arrow.columntable |> DataFrame
+arrowfiles = glob("*",joinpath(@__DIR__,"../data/DVV-90-DAY-COMP/$freqmin-$freqmax/"))
 
 
-# Oct 2004 - May 2005 
+################# overall analysis ##############
+# plot variability in dv/v
+crap =Array{Float64}(undef, length(arrowfiles))
+slat =Array{Float64}(undef, length(arrowfiles))
+slon =Array{Float64}(undef, length(arrowfiles))
+for jj in 1:length(arrowfiles)
+    # read dv/v for each station 
+    DVV = Arrow.Table(arrowfiles[jj]) |> Arrow.columntable |> DataFrame
+    crap[jj]=std(DVV[:,:DVV])
+
+    # get station name and location 
+    netsta = replace(basename(arrowfiles[jj]),".arrow"=>"")
+    net, sta = split(netsta,".")
+    staind = findfirst(CAdf[!,:Station] .== sta)
+    if isnothing(staind)
+        println("No lat/lon for $netsta")
+        continue 
+    end
+    slat[jj] = CAdf[staind,:Latitude]
+    slon[jj] = CAdf[staind,:Longitude]
+end
+Plots.scatter(slon,slat,zcolor=crap,
+title="STD(dv/v)",color=:bilbao,markeralpha=1,
+colorbar_title="",legend=false,colorbar=true,clim=(0,0.5),
+xlim=(-125,-115),ylim=(32,42))
+savefig("../data/FINAL-FIGURES/scatter_dvvstd.png")
+
+
+
+########################## Oct 2004 - May 2005  #################
 maxgap = 15 # maximum data gap [days]
 mindays = 100 # minimum number of days needed for analysis 
-gray = makecpt(color=150, range=(-10000,10000), no_bg=:true);
-arrowfiles = glob("*",joinpath(@__DIR__,"../data/DVV-90-DAY-COMP/$freqmin-$freqmax/"))
+# gray = makecpt(color=150, range=(-10000,10000), no_bg=:true);
 ΔDVVdf = DataFrame()
-
 for jj in 1:length(arrowfiles)
     # read dv/v for each station 
     DVV = Arrow.Table(arrowfiles[jj]) |> Arrow.columntable |> DataFrame
@@ -226,23 +211,9 @@ for jj in 1:length(arrowfiles)
     end
     lonind = argmin(abs.(plon .- stalon))
     latind = argmin(abs.(plat .- stalat))
-    precip = ppt[latind,lonind,:]
-    tpminind = findfirst(tppt .== DVV[1,:DATE])
-    tpmaxind = findlast(tppt .== DVV[end, :DATE])
+    precip =  cummprecip[latind,lonind,4]./precipmean[latind,lonind]
 
-    println("got some precip")
-    octs = Date(2004,10,1):Year(1):Date(2005,10,1)
-    juns = Date(2005,6,1):Year(1):Date(2006,6,1)
-    cumprecip = zeros(eltype(precip),length(octs))
-    for dd = 1:length(octs)-1
-        ind3 = findall((tppt .> octs[dd]) .& (tppt .< juns[dd]))
-        if length(ind3)==0
-            continue
-        else
-         cumprecip[dd] = sum(precip[ind])/1000
-        end
-    end
-    println(cumprecip[1])
+    # println(precip)
 
 
     # println("")
@@ -298,70 +269,42 @@ for jj in 1:length(arrowfiles)
         STA=sta,
         LON=stalon,
         LAT=stalat,
-        cumprecip=cumprecip[1]
+        cumprecip=precip
     )
     append!(ΔDVVdf,df)
 end
 
-# plot with GMT 
-minlon = minimum(ΔDVVdf[:,:LON]) - 0.5
-maxlon = maximum(ΔDVVdf[:,:LON]) + 0.5
-minlat = minimum(ΔDVVdf[:,:LAT]) - 0.5
-maxlat = maximum(ΔDVVdf[:,:LAT]) + 0.5
-
-
-# plot variability in dv/v
-crap =Array{Float64}(undef, length(arrowfiles))
-slat =Array{Float64}(undef, length(arrowfiles))
-slon =Array{Float64}(undef, length(arrowfiles))
-for jj in 1:length(arrowfiles)
-    # read dv/v for each station 
-    DVV = Arrow.Table(arrowfiles[jj]) |> Arrow.columntable |> DataFrame
-    crap[jj]=std(DVV[:,:DVV])
-
-    # get station name and location 
-    netsta = replace(basename(arrowfiles[jj]),".arrow"=>"")
-    net, sta = split(netsta,".")
-    staind = findfirst(CAdf[!,:Station] .== sta)
-    if isnothing(staind)
-        println("No lat/lon for $netsta")
-        continue 
-    end
-    slat[jj] = CAdf[staind,:Latitude]
-    slon[jj] = CAdf[staind,:Longitude]
-end
-Plots.scatter(slon,slat,zcolor=crap,
-title="STD(dv/v)",color=:bilbao,markeralpha=1,
-colorbar_title="",legend=false,colorbar=true,clim=(0,0.5),
-xlim=(-125,-115),ylim=(32,42))
-savefig("../data/FINAL-FIGURES/scatter_dvvstd.png")
-
 
 # plot sensitivity of dv/v to preciptation
-Plots.scatter(ΔDVVdf[:,:LON],ΔDVVdf[:,:LAT],zcolor=-ΔDVVdf[:,:P2P]./ΔDVVdf[:,:dvvstd],
-title="Peak2Peak / STD ",color=:bilbao,markeralpha=1,
-colorbar_title="",legend=false,colorbar=true,clim=(1,5))
-savefig("../data/FINAL-FIGURES/scatter_sensitivity_of_dv.png")
 
-# plot peak2peak
-Plots.scatter(ΔDVVdf[:,:LON],ΔDVVdf[:,:LAT],zcolor=-2*ΔDVVdf[:,:P2P],
-    title="Peak2Peak drop vs total precip",color=:bilbao,markeralpha=2*ΔDVVdf[:,:cumprecip],
-    colorbar_title="",legend=false,colorbar=true,clim=(-1,1))
-savefig("../data/FINAL-FIGURES/scatter_plot_p2p_precip_winter2005.png")
+crap=-ΔDVVdf[:,:P2P]./ΔDVVdf[:,:dvvstd]
+crap2=cummprecip[:,:,4]./precipmean
+Plots.heatmap(plon,plat,-log10.(cummprecip[:,:,4]./precipmean),
+xlim=(-125,-115),clim=(-0.7,0.7),color=:bluesreds,alpha=0.75)
+Plots.scatter!(ΔDVVdf[crap.>0,:LON],ΔDVVdf[crap.>0,:LAT],zcolor=-log10.(crap[crap.>0]),
+title="Peak2Peak / STD ",color=:bluesreds,markeralpha=1,
+colorbar_title="",legend=false,colorbar=true)
+savefig("../data/FINAL-FIGURES/dvv_2004.png")
+
+# # plot peak2peak
+# Plots.scatter(ΔDVVdf[:,:LON],ΔDVVdf[:,:LAT],zcolor=-2*ΔDVVdf[:,:P2P],
+#     title="Peak2Peak drop vs total precip",color=:bilbao,markeralpha=2*ΔDVVdf[:,:cumprecip],
+#     colorbar_title="",legend=false,colorbar=true,clim=(-1,1))
+# savefig("../data/FINAL-FIGURES/scatter_plot_p2p_precip_winter2005.png")
+
+# Plots.scatter(ΔDVVdf[:,:cumprecip],-ΔDVVdf[:,:P2P]./ΔDVVdf[:,:dvvstd])
 
 
 
 
 
 
-########################################
+######################################### 2012 - 2016 ###########################
 
-# 2012 - 2016 
+
 maxgap = 180 # maximum data gap [days]
 mindays = 3 * 365 # minimum number of days needed for analysis 
-arrowfiles = glob("*",joinpath(@__DIR__,"../data/DVV-90-DAY-COMP/$freqmin-$freqmax/"))
 ΔDVVdf = DataFrame()
-
 for jj in 1:length(arrowfiles)
     # read dv/v for each station 
     DVV = Arrow.Table(arrowfiles[jj]) |> Arrow.columntable |> DataFrame
@@ -397,6 +340,9 @@ for jj in 1:length(arrowfiles)
     lonind = argmin(abs.(pptlon .- fitdf[fitind,:LON]))
     latind = argmin(abs.(pptlat .- fitdf[fitind,:LAT]))
     temp = tmean[latind,lonind,:]
+    if maximum(temp)<1
+        continue
+    end
     temp .-= mean(temp)
     tempfilt = smooth_withfiltfilt(temp,window_len=45)
     tempind = findall(DVV[1,:DATE] .<= ttmean .<= DVV[end,:DATE])
@@ -414,15 +360,22 @@ for jj in 1:length(arrowfiles)
     
     # create linear model 
     data = DataFrame(X=tDVV ./ tDVV[end],Y=DVV[ind,:DVV])
+    # G = ones(2,length(tDVV))
+    # G[2,:] = data[:,:X]
+    # m=pinv(G)'*data[:,:Y]
+    # ols = lm(@formula(Y ~ X), data, wts=DVV[ind,:CC] )
     ols = lm(@formula(Y ~ X), data, wts=DVV[ind,:CC] .^ 3)
-
-    # get relevant data 
     inter, slope = coef(ols)
     rtwo = r2(ols)
     interSTD, slopeSTD = stderror(ols)
     mindate = DVV[ind[1],:DATE]
     maxdate = DVV[ind[end],:DATE]
 
+    # Plots.plot(data[:,:X],data[:,:Y])
+    # Plots.plot!(data[:,:X],inter.+slope.*data[:,:X])
+    # Plots.plot!(data[:,:X],m[1].+m[2].*data[:,:X])
+    # Plots.savefig("slope_2012_2016_$netsta.png")
+    # sleep(10)
     # create dataframe
     df = DataFrame(
         MINDATE=mindate,
@@ -442,14 +395,23 @@ for jj in 1:length(arrowfiles)
     append!(ΔDVVdf,df)
 end
 
-#####################
-# 2005 - 2020
+
+Plots.heatmap(plon,plat,-log10.(precipmean_drought[:,:]./precipmean),
+xlim=(-125,-115),clim=(-0.3,0.3),color=:bluesreds)
+Plots.scatter!(ΔDVVdf[:,:LON],ΔDVVdf[:,:LAT],zcolor=crap=ΔDVVdf[:,:SLOPE],
+title="Slope of dv/v and precipitation deficit",color=:bluesreds,markeralpha=1,
+colorbar_title="",legend=false,colorbar=true)
+savefig("../data/FINAL-FIGURES/dvv_2012_2016.png")
+
+
+
+
+############################ LA Area  # 2005 - 2020 ###################################
+
 maxgap = 2*365 # maximum data gap [days]
 mindays = 13 * 365 # minimum number of days needed for analysis 
-
-arrowfiles = glob("*",joinpath(@__DIR__,"../data/DVV-90-DAY-COMP/$freqmin-$freqmax/"))
+# arrowfiles = glob("*",joinpath(@__DIR__,"../data/DVV-90-DAY-COMP/$freqmin-$freqmax/"))
 ΔDVVdf = DataFrame()
-
 for jj in 1:length(arrowfiles)
     # read dv/v for each station 
     DVV = Arrow.Table(arrowfiles[jj]) |> Arrow.columntable |> DataFrame
@@ -528,11 +490,27 @@ for jj in 1:length(arrowfiles)
     append!(ΔDVVdf,df)
 end
 
+
 # plot with GMT 
-minlon = minimum(ΔDVVdf[:,:LON]) - 0.5
-maxlon = maximum(ΔDVVdf[:,:LON]) + 0.5
-minlat = minimum(ΔDVVdf[:,:LAT]) - 0.5
-maxlat = maximum(ΔDVVdf[:,:LAT]) + 0.5
+minlon = -121#minimum(ΔDVVdf[:,:LON]) - 0.5
+maxlon = -116#maximum(ΔDVVdf[:,:LON]) + 0.5
+minlat = 33#minimum(ΔDVVdf[:,:LAT]) - 0.5
+maxlat = 35#maximum(ΔDVVdf[:,:LAT]) + 0.5
+
+Plots.heatmap(plon,plat,-log10.(precipmean_longterm[:,:]./precipmean),
+xlim=(minlon,maxlon),clim=(-0.1,0.1),ylim=(minlat,maxlat),color=:bluesreds,
+    aspect_ratio=:equal)
+Plots.scatter!(ΔDVVdf[:,:LON],ΔDVVdf[:,:LAT],zcolor=crap=ΔDVVdf[:,:SLOPE]/3,
+title="Slope of dv/v and precipitation deficit",color=:bluesreds,markeralpha=1,
+colorbar_title="",legend=false,colorbar=true)
+savefig("../data/FINAL-FIGURES/dvv_2005_2020_LA.png")
+
+
+
+
+################## GMT PLOTS ####################
+
+
 
 # plot Δdv/v 
 colorlimit=0.3
@@ -638,6 +616,19 @@ for jj in 1:length(arrowfiles)
     )
     append!(ΔDVVdf,df)
 end
+
+
+
+
+# plot with GMT 
+minlon = minimum(ΔDVVdf[:,:LON]) - 0.5
+maxlon = maximum(ΔDVVdf[:,:LON]) + 0.5
+minlat = minimum(ΔDVVdf[:,:LAT]) - 0.5
+maxlat = maximum(ΔDVVdf[:,:LAT]) + 0.5
+
+
+
+
 
 # plot with GMT 
 minlon = -122
